7654eef3f6da9bb97414073dab6d170c82ffaad7
Danh Nguyen <myemail@example.com>
2023-10-25 00:38:53 +0700

    Hello Wolrd in php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Tutorial</title>
+</head>
+
+<body>
+    <h1>PHP Tutorial</h1>
+    <?php
+    $name = 'Danh Nguyen';
+    echo 'Hello World' . ' ' . $name;
+    ?>
+</body>
+
+</html>
